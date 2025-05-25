805a65b3aad2e517af6e90052ce1f8fe992f2ee2
4simpleproblems <4simpleproblems@gmail.com>
2025-05-25 16:45:43 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,10 +1,13 @@
 self.__uv$config = {
-    prefix: '/active/go/',
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+  prefix: '/active/go/',
+  bare: 'https://proxy.cors.sh/https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/active/uv/uv.handler.js',
+  bundle: '/active/uv/uv.bundle.js',
+  config: '/active/uv/uv.config.js',
+  sw: '/active/uv/uv.sw.js',
+  headers: {
+    'x-cors-api-key': 'your-api-key-here'
+  }
 };
