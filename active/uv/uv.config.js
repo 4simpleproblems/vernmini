30d7bbcc8e55aab490711e779abe798b4d25672d
4simpleproblems <4simpleproblems@gmail.com>
2025-05-25 17:31:08 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://thingproxy.freeboard.io/fetch/',
+    bare:'https://jsfiddle.net/1d8cwqo0/1/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
