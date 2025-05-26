cabf957acdb81a4584f05efbe80eaf2ee68f3ceb
4simpleproblems <4simpleproblems@gmail.com>
2025-05-25 22:16:40 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://cors-anywhere.herokuapp.com/corsdemo/',
+    bare:'https://ultravioletsw.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
