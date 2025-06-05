9639a2e98aba97b1d1218124b9bbfa26ffee8ae7
4simpleproblems <4simpleproblems@gmail.com>
2025-06-05 02:17:07 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/active/go/",
-    bare: "https://vernmax-bare.netlify.app/", // You can replace this with your own Bare server instance if you have one
+    bare: "https://r2fh2387hv2b2rfbiz.site/", // You can replace this with your own Bare server instance if you have one
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/active/uv/uv.handler.js",
